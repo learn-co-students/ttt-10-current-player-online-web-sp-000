ec242480879b3a7ad392f829ebea3253801d9dff
courtney persons <personscourtney@gmail.com>
2018-09-27 15:34:56 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,9 +10,11 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+turn_count(board) % 2 == 0 ? "X" : "O"
+
+  # if turn_count(board) % 2 == 0
+  #   return "X"
+  # else
+  #   return "O"
+  # end
 end

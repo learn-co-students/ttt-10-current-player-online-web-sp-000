e3bb809b7f9232e747e6bae6ff38ee59b7a68adb
Sam Tamburri <tamburrisam@gmail.com>
2018-09-29 00:32:15 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,24 @@
+def turn_count(board)
+  counter = 0 
+  board.each do | position | 
+    if position == "X" || position == "O"
+      counter +=1 
+    end
+  end
+  counter
+end
+  
+  
+  
+  
+
+
+
+def current_player(board)
+    num = turn_count(board)
+    if num % 2 == 0
+      return "X"
+    else
+      return "O" 
+    end 
+end 

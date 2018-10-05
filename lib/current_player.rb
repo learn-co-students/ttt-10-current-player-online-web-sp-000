e2557fb8d0305c40a6cf8a0de8f2f91d9a8599b6
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-05 05:23:39 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == 'X' || element == 'O'
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board).odd?
+    return 'O'
+  else
+    return 'X'
+  end
+end

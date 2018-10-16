788315be7a95a1e7e794aa80036cd8ede9de5feb
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-16 03:15:26 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  player = turn_count(board).odd? ? "O" : "X"
+  return player
+end

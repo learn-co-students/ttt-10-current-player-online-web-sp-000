1a47e0c0ba3fc6b2d6269e046698979c3906ba3a
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-02-19 01:14:48 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position != " " && position != ""
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  board.each do |turn|
+    if turn_count(board) % 2 == 0
+      return "X"
+    else
+      return "O"
+    end
+  end
+end

fcedc91f0bc225addefa99c3a4d6c85b60706bcb
Marc Poppy <poppy.marc@gmail.com>
2019-04-25 04:58:37 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+
+board = [" ", " ", " ", "", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+  count = 0
+  board.each do |board|
+    if board != " "
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+#use turn count to determine if it's x or o
+# if even it is X, if it has remainder 0
+# if odd it is O, if it has remainder 1
+  if turn_count(board) % 2 == 0
+    return "X"
+  elsif turn_count(board) % 2 == 1
+    return "O"
+  end
+end

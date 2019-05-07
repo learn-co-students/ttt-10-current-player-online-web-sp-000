823648490a29bf4c29e5095d5bffec0cff87b363
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-07 02:29:02 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns +=1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  number_of_turns = turn_count(board)
+  if number_of_turns % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end

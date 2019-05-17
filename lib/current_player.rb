057987ae6c43a479303cd78407d49ec464ca5f6f
Connor Cowan <ccowan003@comcast.net>
2019-05-17 03:21:40 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,23 @@
 #This method takes in argument board array, returns number of turns that have been played
 
 def turn_count(board)
-  counter = 0
-  while counter < 9
-    counter += 1
-end
-
-def current_player()
-
-def current_player(board)
-
-  if turn_count(board) % 2 == 0
-    return "X"
-  else return "O"
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
   end
+  turns       #find out how useful this works...
 end
+
+  #counter = 0
+  #while counter < 9
+      #counter += 1
+    #end
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+  #if turn_count(board) % 2 == 0
+    #return "X"
+  #else return "O"
+  #end

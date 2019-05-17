0e76b6b5facabf993129fbf6438d4d2a37f6912a
Connor Cowan <ccowan003@comcast.net>
2019-05-17 04:19:48 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,24 +1,17 @@
-#This method takes in argument board array, returns number of turns that have been played
+#turn_count takes in argument board array, returns number of turns that have been played
+#current_player method takes in an argument of board array and uses #turn_count method to determine if it is "X"'s turn or "O"'s
 
 def turn_count(board)
-  turn_count = 0
-  board.each do |token|                 #iterate each element on board. token is
+  turns = 0                             #initialize turn_count variable starting at 0
+  board.each do |token|                 #iterate each element on board. token is... 
     if token == "X" || token == "O"     #if tokens are either X or O add to turn count
-      turn_count += 1
+      turns += 1
     end
   end
-  turn_count                      #turn_count call for .... find out how useful this works...
-end
-
-  #counter = 0
-  #while counter < 9
-      #counter += 1
-    #end
+  return turns                           #returns turns. Listed in lab requirements. "Control return value of a method that uses iteraton in body"
+end                                      #This return could also be just: turns. However including 'returns' helps understanding
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"    #call #turn_count. Ternary operators ? : looks for true/false values then executes
 end                                         #using a modulo. if condition is true ? then "X" : otherwise value "O"
-  #if turn_count(board) % 2 == 0
-    #return "X"                             #Refactor for .even? .odd? could work here too in conditions
-  #else return "O"
-  #end
+                                            #Refactor for .even? .odd? could work here too in conditions

812f150e8f721d37e271342cdc2869677dbcb7e5
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-11 13:33:25 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+
 def turn_count(board)
   count = 0
   board.each do |space|
@@ -6,4 +7,12 @@ def turn_count(board)
     end
   end
   count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
 end

7a1f880ac09bd4af2395b1597cee83b27463039a
Amy Fong <amyhfong@gmail.com>
2019-07-14 20:45:53 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,6 @@ def turn_count(board)
 end
 
 def current_player(board)
-  # if turn_count(board) % 2 == 0
-  #   current_player = "X"
-  # else
-  #   current_player = "O"
-  # end
+  # turn_count(board) % 2 == 0 ? "X" : "O"
   turn_count(board).even? ? "X" : "O"
 end

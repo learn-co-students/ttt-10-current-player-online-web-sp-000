24c27f3f617837e80290faba3f3793ff0889f4e9
wei chen <weichen9083@gmail.com>
2019-08-21 23:51:13 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count(board)
+  counter = 0 
+  board.each do |number| 
+    if number == "X" || number == "O"
+      counter +=1 
+    else
+      
+    end
+  end 
+  return counter
+end 
+
+
+
+
+def current_player(board)
+  if turn_count(board)%2 == 0 
+    return "X"
+  else 
+    return "O"
+  end 
+end 

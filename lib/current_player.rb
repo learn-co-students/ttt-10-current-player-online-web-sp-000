19cd2f40388e54aa57a8f1cfa330133b95e6393b
Aoune Syed <aoune007@gmail.com>
2019-08-28 05:40:37 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,9 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+def turn_count(board)
+    board.each do |character| #character == "X" || character == "O"
+    counter = 0
+end
+  if character == "X" || character == "O"
+      counter += 1
+  end
+end

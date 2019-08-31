c117d69799ee8302cafd44f0a4f62c44f2171198
Blythe Pollard <blytheepollard@gmail.com>
2019-08-31 16:22:14 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,12 +2,19 @@
 def turn_count(board) 
  counter = 0 #counter at 0
     board.each do |space| #check array starting at 0
-      if space == "X" || space == "O"
-      counter += 1 
+      if space == "X" || space == "O" #if space is taken
+      counter += 1 #then count one more
     end
-     #return amount of turns taken
-    end
+  end
+  counter
 end  
 
 #returns x or o based on how many turns played
 def current_player(board)
+  turn_count(board)
+    if turn_count(board).even?
+       "X"
+    else 
+       "O"
+  end
+end

a33ba1b08cecf7aeb1ba87a193963de710646cae
wolfene2001 <wolfene2001@yahoo.com>
2019-09-13 23:10:05 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   # board == ["X", "O", "X", "", "X"]
   turn = 0
   board.each do |token|
-    if token = "X" || token = "O"
+    if token == "X" || token == "O"
     turn += 1
 
   end
@@ -12,8 +12,8 @@ end
 
 def current_player(board)
   if turn_count(board)  % 2 != 0
-    token = "X"
-  elsif turn_count(board)  % 2 == 0
     token = "O"
+  elsif turn_count(board)  % 2 == 0
+    token = "X"
   end
 end

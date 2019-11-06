f84fc2cb7380a6adb87442edc55fe8102d1ec4ff
Sweet Van Loan <sweet@sweetvanloan.com>
2019-11-06 01:01:48 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  puts (turn_count)
+  counter = 0 
+  while count <= board.length 
+  counter += 1 
+  if counter >=9
+  puts "End of Game"
+  end
+end 
+  
+def current_player(board)
+  current_player.even?
+  if TRUE
+    current_player = "O"
+  else current_player = "X"
+  end
+end

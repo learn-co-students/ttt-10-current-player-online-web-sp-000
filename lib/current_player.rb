1c678264ca55fccbbaa5c08f284f9cd093328ee6
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-18 23:33:16 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,12 +8,18 @@ def turn_count(value)
   return count 
 end 
 
-
 def current_player(board)
   count = turn_count(board)
-  if count == 0 || count % 2 == 0 
-    return "X"
-  else
-    return "O"
-  end
-end 
+  current_player = count == 0 || count.even? ? "X" : "O"
+end
+
+# Note: the code below is the same as the one above.  Cool! Bonus work above.
+
+#def current_player(board)
+#  count = turn_count(board)
+#  if count == 0 || count.even?
+#    return "X"
+#  else
+#    return "O"
+#  end
+#end 

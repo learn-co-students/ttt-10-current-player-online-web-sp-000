d93a329d0108cafacdcd45ca3e77bb1eadab8d13
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-03-04 01:14:17 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,17 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 def turn_count(board)
   counter = 0
-  board.each do |counter|
-    return counter
-    counter += 1
+  board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    return "X"
+  else turn_count(board) % 2 == 1 
+    return "O"
   end
 end

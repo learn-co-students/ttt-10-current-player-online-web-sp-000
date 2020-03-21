eee4298e29d7e037c511c0d2cccaaa1808478d6d
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-21 17:21:50 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+
+  counter = 0
+
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+
+def current_player(board)
+  if turn_count(board) % 2 != 0
+    "O"
+  else turn_count(board) % 2
+    "X"
+  end
+end

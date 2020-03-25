32e101aa32da9c615f5384b41233644315b0986e
Evan Burton <evan@heyevan.com>
2020-03-25 07:18:56 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,10 +8,6 @@ def turn_count(board)
   move_count
 end
 
-def current_player (board)
-  if turn_count(board) % 2 == 0
-  return "X"
-else
-  return "O"
+def current_player(board)
+    turn_count(board) % 2 == 0 ? "X" : "O"
   end
-end

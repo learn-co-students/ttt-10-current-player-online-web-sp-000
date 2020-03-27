259981526d5058ae1001dcbcf16a228394b58239
Martin Moy <martinmoy1991@gmail.com>
2020-03-27 16:13:02 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,21 @@
 def turn_count(board)
-  turn_count = 0
-  board.each do |value|
-    if value == "X" || value == "O"
-      turn_count += 1
+  turn = 0
+  board.each do |spot|
+    if spot== "X" || spot == "O"
+      turn += 1
     end
   end
-  return turn_count
+    return turn
 end
 
 def current_player(board)
   turn_count(board).even? ? "X": "O"
 end
+#
+#def current_player(board)
+#  if turn_count(board).even?
+#    return "X"
+#  else
+#    return "O"
+#  end
+#end

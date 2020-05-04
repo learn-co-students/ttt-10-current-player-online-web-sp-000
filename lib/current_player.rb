1b2c62305b7fbd6d986e06cca7d2f7a1cd0ca674
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-04 00:04:15 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,24 @@
+
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+      puts "#{counter}"
+    end
+  end
+
+  counter
+end
+
+
+def current_player(board)
+  turn_num = turn_count(board) + 1
+  if turn_num.even? == true
+    return "O"
+  else
+    return "X"
+  end
+end

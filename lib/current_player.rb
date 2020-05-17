13b69d009d69fc3f52786d1fd25b21c2827444e0
Claire Bullock <bullockcm13@gmail.com>
2020-05-17 19:25:21 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count(board)
+  turns = 0 
+  board.each do |player|
+    if player == "X" || player == "O"
+      turns += 1 
+    end 
+  end
+  return turns 
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 == 0 
+    return "X"
+  else 
+    return "O"
+  end
+end
+
+  
+  
+  

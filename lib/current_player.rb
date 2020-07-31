0b2478ebcfecbc716bb00f4a9caf4e46e13ee104
shawn campbell <shacoca@yahoo.com>
2020-07-31 17:53:47 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,17 @@
 def turn_count(board)
+  number_of_turns = 0
+  board.each do |space|
+    if (space == "X" || space == "O")
+      number_of_turns += 1
+    end
+  end
+  number_of_turns
 end
 
 def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
 end

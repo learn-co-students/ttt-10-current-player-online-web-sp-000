0f4a09813fbabcc92c1e49db3db8de49a4f89c53
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-15 01:30:13 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,26 @@
+
+def turn_count(board)
+  counter = 0
+
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+      puts "#{counter}"
+    end
+  end
+counter
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    return "X"
+  else
+    return "O"
+  end
+end
+    
+  
+      
+    
+  
+  

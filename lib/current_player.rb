b3801ae6657c15d67fb6b9d4149aa172db252704
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-08 01:24:45 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,5 @@
 def turn_count(board)
-  empty_cells = 0
-  board.each do |cell|
-    if cell != 'X' && cell != 'O'
-      empty_cells += 1
-    end
-  end
-  board.length - empty_cells
+  board.count {|cell| cell == "X" || cell == "O"}
 end
     
 def current_player(board)

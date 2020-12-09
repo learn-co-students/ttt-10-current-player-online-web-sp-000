c6678957d13901c5527c99e1b8d95913e7d5c3bc
Romano Livi <romano_livi@outlook.com>
2020-12-09 19:55:33 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  counter = 0 
+  board.each{|location|
+  if location == "X" || location == "O"
+    counter += 1 
+  end
+  }
+  return counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0? "X" : "O"
+end
+  
+
+
+
+
+
+

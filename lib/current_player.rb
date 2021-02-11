96eb865d62b50b3e800e64b4f67391b78cb05865
meaghan jennings <meaghanljennings@gmail.com>
2021-02-11 14:39:29 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,20 @@
 def turn_count(board)
   turns = 0
   board.each do |space|
+    #if my condition is met 
     if space == "X" || space == "O"
-      turns += 1
+      turns +=  1
     end
   end
   return turns
-end
-  
+end 
+
 def current_player(board)
   turns = turn_count(board)
-  if turns % 2 == 0
+  if turns.even?   #could also use if turns % 2 == 0
     return "X"
   else
     return "O"
   end
+  
 end

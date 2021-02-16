ec60118df66256f4a4f0d1cddc22955a591b765f
Muneeb Dahab <muneeb.dahab@outlook.com>
2021-02-16 16:29:20 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/current_player.rb'
+
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
+end
+

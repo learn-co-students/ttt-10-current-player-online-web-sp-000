d5d1d95189e3d99aefa288689d3dc675d147e094
Lj <tnedutsl457892@gmail.com>
2021-02-21 07:59:01 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,9 +11,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if (turn_count(board).even == true)
+  if (turn_count(board) / 2 == 0)
     return "X"
-  else
+  elsif turn_count(board) / 2 != 0
     return "O"
   end
 end

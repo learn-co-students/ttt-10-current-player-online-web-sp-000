205fbf05f3a84c9a86eeb5f64368099fce861c3d
Lj <tnedutsl457892@gmail.com>
2021-02-21 07:35:19 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,17 @@
 def turn_count(board)
   counter = 0
-  board.each do |char|
-  if char = "X" || "O"
-    return counter += 1
+  board.each do |element|
+    if (element == "X" || element =="O")
+      counter += 1
+    else
+      # do nothing
+    end
   end
+  return counter
 end
 
 def current_player(board)
-  if turn_count / 2 = 0
+  if (turn_count(board) / 2 = 0)
     return "X"
   else
     return "O"

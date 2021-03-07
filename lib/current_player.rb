4f0c23720da8c9e557cea6b387d5e7e711f57e39
Guosa_Edebiri <gedebiri@gmail.com>
2021-03-07 05:30:41 -0600

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+
+
+
+def turn_count(board)
+    counter = 0
+    board.each do |board|
+        if (board == "X") || (board == "O")
+            counter += 1
+        end
+    end
+    counter
+end
+
+def current_player(board)
+    if turn_count(board).even?
+        return "X"
+    elsif turn_count(board).odd?
+        return "O"
+    end
+end
